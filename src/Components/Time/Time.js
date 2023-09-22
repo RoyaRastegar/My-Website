7da1568bd5369feda630b4629569382747a7b1d4
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2023-09-22 12:20:13 +0800

    Time Component

---
+++ src/Components/Time/Time.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+const Time = () => {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+  }, []);
+  return (
+    <div>
+      {" "}
+      <h5>Time : {time} </h5>
+    </div>
+  );
+};
+
+export default Time;
