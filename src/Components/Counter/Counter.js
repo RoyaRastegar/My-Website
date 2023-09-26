fcaa103452e0eb9c5fa19452893c8a9d3d891189
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2023-09-26 13:30:19 +0800

    add Counter component

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,11 +1,50 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "./Counter.css";
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handelAdd(e) {
+    e.preventDefault();
+    setCount((c) => c + step);
+  }
+  function handelMines(e) {
+    e.preventDefault();
+    setCount((c) => c - step);
+  }
+  function handelAddStep(e) {
+    e.preventDefault();
+    setStep((s) => s + 1);
+  }
+  function handelMinesStep(e) {
+    e.preventDefault();
+    setStep((s) => s - 1);
+  }
   return (
     <>
-      <h1>Counter</h1>
+      <div className="counter">
+        <div className="heder">
+          <h1>Counter</h1>
+        </div>
+        <div className="buttons">
+          <button className="button" onClick={handelMinesStep}>
+            -
+          </button>
+          <span className="span">Step : {step} </span>
+          <button className="button" onClick={handelAddStep}>
+            +
+          </button>
+          <button className="button" onClick={handelMines}>
+            -
+          </button>
+          <span className="span">Count : {count} </span>
+          <button className="button" onClick={handelAdd}>
+            +
+          </button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
